2eec2cfca3a272d20fffc8d3fe1704b925f2ecb4
algorythmist <dimitri@tecacet.com>
2019-01-01 13:39:09 -0800

    use ribbon for load balancing

--- sardine-booking/src/main/java/com/tecacet/sardine/booking/component/FareServiceProxy.java
+++ sardine-booking/src/main/java/com/tecacet/sardine/booking/component/FareServiceProxy.java
@@ -2,16 +2,18 @@ package com.tecacet.sardine.booking.component;
 
 import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//TODO: how to externalize this url?
-@FeignClient(name="fares-proxy", url="localhost:8080/fares")
+
+@FeignClient(name="fares-proxy")
+@RibbonClient(name="fares")
 public interface FareServiceProxy {
 
-    @RequestMapping(value="/get", method = RequestMethod.GET)
+    @RequestMapping(value="fares/get", method = RequestMethod.GET)
     Fare getFare(@RequestParam(value="flightNumber") String flightNumber,
                  @RequestParam(value = "flightDate") String flightDate);
 }
